64cc8eb125d01ca81ad9a085a270502e88d989fb
brenim-developer <brenodolcinotti@gmail.com>
2025-08-25 22:51:41 -0300

    NEW: Tabuleiro controla estado dos botões

--- src/App.js
+++ src/App.js
@@ -11,22 +11,30 @@ function Square({valor, onSquareClick}){
 }
 
 export default function Tabuleiro(){
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  function handleClick(i){
+    const nextSquares = squares.slice();
+    nextSquares[i] = "X";
+    setSquares(nextSquares);
+  }
+
   return(
     <div>
       <div>
-        <Square />
-        <Square />
-        <Square />
+        <Square valor={squares[0]} onSquareClick={()=>{handleClick(0)}}/>
+        <Square valor={squares[1]} onSquareClick={()=>{handleClick(1)}}/>
+        <Square valor={squares[2]} onSquareClick={()=>{handleClick(2)}}/>
       </div>
       <div>
-        <Square />
-        <Square />
-        <Square />
+        <Square valor={squares[3]} onSquareClick={()=>{handleClick(3)}}/>
+        <Square valor={squares[4]} onSquareClick={()=>{handleClick(4)}}/>
+        <Square valor={squares[5]} onSquareClick={()=>{handleClick(5)}}/>
       </div>
       <div>
-        <Square />
-        <Square />
-        <Square />
+        <Square valor={squares[6]} onSquareClick={()=>{handleClick(6)}}/>
+        <Square valor={squares[7]} onSquareClick={()=>{handleClick(7)}}/>
+        <Square valor={squares[8]} onSquareClick={()=>{handleClick(8)}}/>
       </div>
     </div>
   );
